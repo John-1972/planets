fccd3df0554540af0081173d41d8d46ef03f4605
John Henderson <johnhenderson@fastmail.fm>
2018-10-11 15:45:42 +0000

    Generated model, modified migration file

---
+++ db/migrate/20181011153313_create_planets.rb
@@ -0,0 +1,11 @@
+class CreatePlanets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :planets do |t|
+      t.string :name
+      t.string :dist_from_sun
+      t.string :eq_diameter
+      t.string :orbit_period	
+      t.timestamps
+    end
+  end
+end
