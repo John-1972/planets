467547fd6b2ba48b6b0f9837e60f48f9602e0f98
John Henderson <johnhenderson@fastmail.fm>
2018-10-18 18:20:48 +0000

    Kind of bodged together some rudimentary, hardwired navigation

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,5 +1,12 @@
 class PlanetsController < ApplicationController
 	def index
-		@planet = Planet.order("RANDOM()").first
+		@planet = Planet.first
+    # @current_planet = @planet
 	end
+
+  def show
+    # @next_planet_id = @current_planet.id + 1
+    @planet = Planet.find(2)
+    # @current_planet = @planet
+  end
 end
