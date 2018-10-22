4983761da20043e81408063c4eaaef54ad08423d
John Henderson <johnhenderson@fastmail.fm>
2018-10-22 16:43:53 +0000

    Got my conditional links to work!

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -4,6 +4,14 @@ class PlanetsController < ApplicationController
 	end
 
   def show
-    @planet = Planet.find(params[:id])
+    @planet = Planet.find(params[:id]) # Get num in the URL & fetch data for planet with that id
+
+    # I need to 'switch off' the previous/next links for the first and last planets, so...
+    if @planet.id != 1
+      @not_first_planet = true # If this isn't the first planet, set boolean to that effect
+    end
+    if @planet.id != 8 
+      @not_last_planet = true # If this isn't the last planet, set boolean to that effect
+    end
   end
 end
